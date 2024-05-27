c78895740e1e035111e5b8bdbf3f4caca761631c
CodingWithAbdou <coder.abdou@gmail.com>
2024-05-27 18:17:23 +0100

    chamge qualityt video to 240 and 3360 and 480 and  720px

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Video;
+use FFMpeg\Coordinate\Dimension;
+use FFMpeg;
+use FFMpeg\Format\Video\X264;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
+// use Illuminate\Support\Str;
 use Intervention\Image\ImageManagerStatic as Image;
 
 class VideoController extends Controller
@@ -33,11 +36,11 @@ class VideoController extends Controller
     {
         $this->validate($request, [
             'title' =>  'required',
-            'image_path' => 'required|image',
-            'video_path' => 'required',
+            'image' => 'required|image',
+            'video' => 'required',
         ]);
 
-        $reandomPath = Str::random(16);
+        $reandomPath = time();
         $videoPath = $reandomPath . '.'  . $request->video->getClientOriginalExtension();
         $imagePath = $reandomPath . '.'  . $request->image->getClientOriginalExtension();
 
@@ -45,13 +48,58 @@ class VideoController extends Controller
         $path = Storage::put($imagePath, $image->stream());
         $request->video->storeAs('/', $videoPath, 'public');
 
-        Video::create([
+        $video =  Video::create([
             'disk' => 'public',
             'title' =>  $request->title,
-            'image' => $imagePath,
-            'video' => $videoPath,
+            'image_path' => $imagePath,
+            'video_path' => $videoPath,
             'user_id' => auth()->id(),
         ]);
+        $format_240 = (new X264('aac', 'libx264'))->setKiloBitrate(500);
+        $format_360 = (new X264('aac', 'libx264'))->setKiloBitrate(900);
+        $format_480 = (new X264('aac', 'libx264'))->setKiloBitrate(1500);
+        $format_720 = (new X264('aac', 'libx264'))->setKiloBitrate(3000);
+
+        $path_240 = '240-' . $video->video_path;
+        $path_360 = '360-' . $video->video_path;
+        $path_480 = '480-' . $video->video_path;
+        $path_720 = '720-' . $video->video_path;
+
+        FFMpeg::fromDisk($video->disk)
+            ->open($video->video_path)
+            ->addFilter(function ($filters) {
+                $filters->resize(new Dimension(426, 240));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($format_240)
+            ->save($path_240)
+
+            ->addFilter(function ($filters) {
+                $filters->resize(new Dimension(640, 360));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($format_360)
+            ->save($path_360)
+
+            ->addFilter(function ($filters) {
+                $filters->resize(new Dimension(854, 480));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($format_480)
+            ->save($path_480)
+
+            ->addFilter(function ($filters) {
+                $filters->resize(new Dimension(1280, 720));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($format_720)
+            ->save($path_720);
+
+        return redirect()->back();
     }
 
     /**
